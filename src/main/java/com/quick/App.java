b9ffddab8a200b40d0fa47e201593af9f783d5e2
Ubuntu <ubuntu@ip-101-0-1-65.ec2.internal>
2022-10-12 08:39:35 +0000

    added

---
+++ src/main/java/com/quick/App.java
@@ -0,0 +1,13 @@
+package com.quick;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
